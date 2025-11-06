d2f170ee7c4a96afb1b10e2b02ba0998bd3fcaa6
ltaoo <litaowork@aliyun.com>
2025-11-06 19:21:33 +0800

    feat: add SlateHistoryModel

--- frontend/src/components/slate/slate.tsx
+++ frontend/src/components/slate/slate.tsx
@@ -7,6 +7,8 @@ import { SlateEditorModel } from "@/biz/slate/slate";
 import { SlateDescendant, SlateDescendantType, SlateOperationType } from "@/biz/slate/types";
 import { SlatePoint } from "@/biz/slate/point";
 import { ButtonCore } from "@/domains/ui";
+import { findNodeWithPath, getNodePath, applyCaretPosition, connect } from "@/biz/slate/connect.web";
+import { deleteTextAtOffset, deleteTextInRange, insertTextAtOffset } from "@/biz/slate/utils/text";
 
 export function SlateView(props: { store: SlateEditorModel }) {
   return <SlateEditable store={props.store} />;
@@ -14,44 +16,14 @@ export function SlateView(props: { store: SlateEditorModel }) {
 // const TEXT_EMPTY_PLACEHOLDER = "&#x2060;<br>";
 // const TEXT_EMPTY_PLACEHOLDER = "<br>";
 const TEXT_EMPTY_PLACEHOLDER = "&nbsp;";
+
 function SlateEditable(props: { store: SlateEditorModel }) {
-  let $editor: HTMLDivElement | undefined;
+  let $input: HTMLDivElement | undefined;
 
   const [state, vm] = useViewModelStore(props.store);
   const [selection, $selection] = useViewModelStore(props.store.ui.$selection);
+  const [history, $history] = useViewModelStore(props.store.ui.$history);
 
-  function getNodePath(targetNode: Element, rootNode: Element) {
-    const path = [];
-    let currentNode = targetNode;
-
-    // 从目标节点向上遍历直到根节点
-    while (currentNode && currentNode !== rootNode) {
-      const parent = currentNode.parentNode;
-      if (!parent) break;
-
-      // 获取当前节点在父节点中的索引
-      const children = Array.from(parent.children);
-      const index = children.indexOf(currentNode);
-
-      if (index !== -1) {
-        path.unshift(index); // 添加到路径开头
-      }
-      // @ts-ignore
-      currentNode = parent;
-    }
-
-    return path;
-  }
-  function findNodeWithPath($elm: Element, path: number[]): Element | null {
-    if (path.length === 0) {
-      return $elm;
-    }
-    const $v = $elm.children[path[0]];
-    if (!$v) {
-      return null;
-    }
-    return findNodeWithPath($v, path.slice(1));
-  }
   function renderText(node: SlateDescendant & { key?: number }): Element | null {
     if (node.type === SlateDescendantType.Text) {
       const $text = document.createElement("span");
@@ -119,151 +91,68 @@ function SlateEditable(props: { store: SlateEditorModel }) {
     }
     return $tmp;
   }
-
-  function applyCaretPosition($editor: Element, start: SlatePoint, end: SlatePoint) {
-    //     const { start, end } = vm.ui.$selection.state;
-    const $node_start = findNodeWithPath($editor as Element, start.path);
-    const $node_end = findNodeWithPath($editor as Element, end.path);
-    if (!$node_start || !$node_end) {
-      return;
-    }
-    const selection = window.getSelection();
-    if (!selection) {
-      return;
-    }
-    const range = document.createRange();
-    range.setStart($node_start.childNodes[0], start.offset);
-    range.setEnd($node_end.childNodes[0], end.offset);
-    selection.removeAllRanges();
-    selection.addRange(range);
-  }
-
   const $btn = new ButtonCore({
     onClick() {
       //       vm.methods.setCaretPosition();
     },
   });
 
-  vm.onAction((operation) => {
-    if (!$editor) {
+  vm.onAction((operations) => {
+    if (!$input) {
       return;
     }
-
-    if (operation.type === SlateOperationType.InsertText) {
-      const $target = findNodeWithPath($editor as Element, operation.path) as Element | null;
-      console.log("[]vm.onAction - SlateOperationType.InsertText", $target, operation.path);
-      if (!$target) {
-        return;
+    for (let i = 0; i < operations.length; i += 1) {
+      const op = operations[i];
+      if (op.type === SlateOperationType.InsertText) {
+        const $target = findNodeWithPath($input as Element, op.path) as Element | null;
+        if (!$target) {
+          return;
+        }
+        console.log("[]vm.onAction - SlateOperationType.InsertText", $target.innerHTML, op.text);
+        $target.innerHTML = insertTextAtOffset($target.innerHTML, op.text, op.offset);
       }
-      if ($target.localName === "br") {
-        $target.parentElement?.replaceChild(renderText(operation.node)!, $target);
-      } else {
-        $target.innerHTML = operation.wholeText;
+      if (op.type === SlateOperationType.RemoveText) {
+        const $target = findNodeWithPath($input as Element, op.path) as Element | null;
+        if (!$target) {
+          return;
+        }
+        console.log("[]vm.onAction - SlateOperationType.DeleteText", $target.innerHTML, op.text, op.offset);
+        const nextText = deleteTextAtOffset($target.innerHTML, op.text, op.offset);
+        $target.innerHTML = nextText === "" ? TEXT_EMPTY_PLACEHOLDER : nextText;
       }
-      applyCaretPosition($editor, vm.ui.$selection.start, vm.ui.$selection.end);
-    }
-    if (operation.type === SlateOperationType.DeleteText) {
-      const $target = findNodeWithPath($editor as Element, operation.path) as Element | null;
-      if (!$target) {
-        return;
+      if (op.type === SlateOperationType.InsertLine) {
+        const $node = renderElement(op.node);
+        if (!$node) {
+          return;
+        }
+        const idx = op.path[0] + 1;
+        if (idx > $input.children.length - 1) {
+          $input.appendChild($node);
+        } else {
+          $input.insertBefore($node, $input.children[idx]);
+        }
       }
-      $target.innerHTML = operation.wholeText === "" ? TEXT_EMPTY_PLACEHOLDER : operation.wholeText;
-      applyCaretPosition($editor, vm.ui.$selection.start, vm.ui.$selection.end);
-    }
-    if (operation.type === SlateOperationType.InsertLine) {
-      const $node = renderElement(operation.node);
-      if (!$node) {
-        return;
-      }
-      let idx = operation.idx + 1;
-      if (idx > $editor.children.length - 1) {
-        $editor.appendChild($node);
-      } else {
-        $editor.insertBefore($node, $editor.children[idx]);
-      }
-      console.log("before apply ", vm.ui.$selection.start.path);
-      applyCaretPosition($editor, vm.ui.$selection.start, vm.ui.$selection.end);
     }
   });
-  onMount(() => {
-    if (!$editor) {
+  vm.onSelectionChange(({ start, end }) => {
+    if (!$input) {
       return;
     }
-    document.addEventListener("selectionchange", (event) => {
-      //       console.log('addEventListener("selectionchange', vm.ui.$selection.dirty);
-      //       if (vm.ui.$selection.dirty) {
-      //         return;
-      //       }
-      vm.methods.handleSelectionChange();
-    });
-    // $editor.addEventListener("mouseup", (event) => {
-    //   vm.methods.getCaretPosition();
-    // });
-    vm.methods.getCaretPosition = function () {
-      const selection = window.getSelection();
-      if (!selection) {
-        return;
-      }
-      if (selection.rangeCount === 0) {
-        return;
-      }
-      const range = selection.getRangeAt(0);
-      const $start = range.startContainer.parentNode as HTMLDivElement | null;
-      const $end = range.endContainer.parentNode as HTMLDivElement | null;
-      if (!$start || !$end) {
-        return;
-      }
-      const offset_start = range.startOffset;
-      const offset_end = range.endOffset;
-      // const path_start = vm.methods.mapNodeWithKey($start.dataset["slate-node-key"]);
-      // const path_end = vm.methods.mapNodeWithKey($end.dataset["slate-node-key"]);
-      console.log("[]getCaretPosition - ", $start);
-      if ($start === $editor) {
-        return;
-      }
-      const path_start = getNodePath($start, $editor);
-      const path_end = getNodePath($end, $editor);
-      if (!path_start || !path_end) {
-        return;
-      }
-      vm.ui.$selection.methods.handleChange({
-        start: {
-          path: path_start,
-          offset: offset_start,
-        },
-        end: {
-          path: path_end,
-          offset: offset_end,
-        },
-        collapsed: range.collapsed,
-      });
-    };
-    vm.methods.setCaretPosition = function (arg: { start: SlatePoint; end: SlatePoint }) {
-      const $node_start = findNodeWithPath($editor as Element, arg.start.path);
-      const $node_end = findNodeWithPath($editor as Element, arg.start.path);
-      if (!$node_start || !$node_end) {
-        return;
-      }
-      const selection = window.getSelection();
-      if (!selection) {
-        return;
-      }
-      const range = document.createRange();
-      range.setStart($node_start.childNodes[0], arg.start.offset);
-      range.setEnd($node_end.childNodes[0], arg.end.offset);
-      selection.removeAllRanges();
-      selection.addRange(range);
-    };
+    applyCaretPosition($input, start, end);
+  });
+  onMount(() => {
+    if (!$input) {
+      return;
+    }
+    connect(vm, $input);
     const $elements = buildInnerHTML(vm.state.children);
-    $editor.appendChild($elements);
-    // console.log(innerHTML);
-    // $editor.innerHTML = innerHTML;
+    $input.appendChild($elements);
   });
 
   return (
     <>
       <div
-        ref={$editor}
+        ref={$input}
         class="overflow-y-auto p-1 border border-2 border-w-fg-3 rounded-md outline-none"
         style={{ "white-space": "pre-wrap" }}
         contenteditable
@@ -293,14 +182,28 @@ function SlateEditable(props: { store: SlateEditorModel }) {
       <div>
         <Button store={$btn}>测试</Button>
       </div>
-      <div>
-        <div>
-          Ln {selection().start.line}, Col {selection().start.offset}
+      <div class="flex gap-4">
+        <div class="flex-1">
+          <div>
+            Ln {selection().start.line}, Col {selection().start.offset}
+          </div>
+          <div>
+            Ln {selection().end.line}, Col {selection().end.offset}
+          </div>
+          <div>{selection().collapsed ? "光标" : "选区"}</div>
         </div>
-        <div>
-          Ln {selection().end.line}, Col {selection().end.offset}
+        <div class="overflow-y-auto flex-1 max-h-[240px]">
+          <For each={history().stacks}>
+            {(stack) => {
+              return (
+                <div>
+                  <div>{stack.type}</div>
+                  <div class="text-[12px] text-w-fg-1">{stack.created_at}</div>
+                </div>
+              );
+            }}
+          </For>
         </div>
-        <div>{selection().collapsed ? "光标" : "选区"}</div>
       </div>
     </>
   );
--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+declare namespace fmt {
+  function Println(...args: any[]): void;
+}
+
+declare global {
+  interface Array<T> {
+    last(): T | undefined;
+  }
+}
